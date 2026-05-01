35642accb313bf02b87f149ef932fbefa3e82565
kandazh <kandazhmail@gmail.com>
2026-05-01 17:24:25 +0530

    build issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -150,6 +150,7 @@ dependencies {
     implementation(libs.okhttp)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
